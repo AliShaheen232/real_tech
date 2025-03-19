6cadc890c18bfce91045fe84208bc69ea6641768
professionalali22 <54819672+AliShaheen232@users.noreply.github.com>
2025-03-19 14:15:16 +0500

    Update FreeREALDistributor.sol

--- hardhat/contracts/FreeREALDistributor.sol
+++ hardhat/contracts/FreeREALDistributor.sol
@@ -8,17 +8,12 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import "@openzeppelin/contracts/utils/Pausable.sol";
 
 contract FreeREALDistributor is Ownable, ReentrancyGuard, Pausable {
-    uint16 public constant DENOMINATOR = 10000;
     uint256 public HARDCAP;
     uint256 public totalClaimed;
+    uint256 claimableAmt;
     IERC20 public real;
-    uint16 public distPercentage; // must be in multiple of 100
 
-    mapping(address => uint256) public userClaimedAmount;
-
-    receive() external payable {}
-
-    fallback() external payable {}
+    mapping(address => bool) public userClaimed;
 
     event REALClaimed(
         address indexed _user,
@@ -30,52 +25,41 @@ contract FreeREALDistributor is Ownable, ReentrancyGuard, Pausable {
 
     constructor(
         address _real,
-        uint256 _hardCAP,
-        uint16 _distPercentage
+        uint256 _claimableAmt,
+        uint256 _hardCAP
     ) Ownable(msg.sender) {
-        require(
-            _distPercentage <= 4900,
-            "Distribution percentage must be <= 49"
-        );
         real = IERC20(_real);
+        claimableAmt = _claimableAmt;
         HARDCAP = _hardCAP;
-        distPercentage = _distPercentage;
     }
 
     function claimREAL() external whenNotPaused nonReentrant {
+        require(!userClaimed[msg.sender], "Free tokens already claimed");
+
         require(
-            userClaimedAmount[msg.sender] == 0,
-            "Free tokens already claimed"
+            real.balanceOf(address(this)) >= claimableAmt,
+            "Contract have less REAL balance"
         );
 
-        uint256 _amount = real.balanceOf(msg.sender);
-        require(_amount > 0, "Zero Real balance!");
-        _amount = (_amount * distPercentage) / DENOMINATOR;
+        uint256 _amount = address(msg.sender).balance;
+        require(_amount > 0, "No ETH balance!");
 
-        require((totalClaimed + _amount) <= HARDCAP, "Hardcap reached");
+        require(claimableAmt > 0, "Set claimable amount");
+        require(totalClaimed < HARDCAP, "Hardcap reached");
 
-        totalClaimed += _amount;
-        userClaimedAmount[msg.sender] = _amount;
+        totalClaimed += claimableAmt;
+        userClaimed[msg.sender] = true;
 
-        SafeERC20.safeTransfer(real, msg.sender, _amount);
+        SafeERC20.safeTransfer(real, msg.sender, claimableAmt);
 
-        emit REALClaimed(msg.sender, _amount, block.timestamp);
+        emit REALClaimed(msg.sender, claimableAmt, block.timestamp);
     }
 
-    function setDistPercentage(uint16 _distPercentage) public onlyOwner {
-        require(
-            _distPercentage <= 4900,
-            "Distribution percentage must be <= 49"
-        ); // @admin - {distPercentage} could be 49% maximum.
-
-        distPercentage = _distPercentage;
-    }
-
-    function pause() public whenNotPaused {
+    function pause() public whenNotPaused onlyOwner {
         _pause();
     }
 
-    function unpause() public whenPaused {
+    function unpause() public whenPaused onlyOwner {
         _unpause();
     }
 
@@ -93,9 +77,5 @@ contract FreeREALDistributor is Ownable, ReentrancyGuard, Pausable {
         SafeERC20.safeTransfer(IERC20(address(real)), msg.sender, amount);
 
         emit REALWithdrawn(amount);
-    }
-
-    function userClaimStatus(address user) public view returns (bool _status) {
-        if (userClaimedAmount[user] > 0) return true;
     }
 }
