3f9a498ec408d673eb74d49afc9242a6eee769c3
professionalali22 <54819672+AliShaheen232@users.noreply.github.com>
2025-03-25 21:44:31 +0500

    Update Vesting.sol

--- hardhat/contracts/Vesting.sol
+++ hardhat/contracts/Vesting.sol
@@ -14,6 +14,7 @@ contract FreeREALDistributor is Ownable, ReentrancyGuard, Pausable {
         uint32 unlockTime;
     }
 
+    address public vestingFund;
     uint256 public vestingFund;
     IERC20 public realToken;
     UnlockDetail[] public ulockDetails;
