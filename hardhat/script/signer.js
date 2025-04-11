9d4970176236f8c36343e669561c4b4ae60c2b89
professionalali22 <54819672+AliShaheen232@users.noreply.github.com>
2025-04-12 00:11:34 +0500

    updated with FS module

--- hardhat/script/signer.js
+++ hardhat/script/signer.js
@@ -1,14 +1,15 @@
 const Web3 = require("web3");
+const fs = require("fs");
 
 const web3 = new Web3.default(
   "https://mainnet.infura.io/v3/335d7bd80d5747f482a8c603c4beae14"
 );
 
 const PVT_KEY =
-  "0x1f806af233b232a325874e13a7ccf63cb65e219165e956a684829cbfe3aac25e"; // @dev - dummy address and private key 0x52C75ad49024dB2474cB8a581F625bee2E14e8E7
+  "0x1f806af233b232a325874e13a7ccf63cb65e219165e956a684829cbfe3aac25e"; // @dev - dummy private key for address 0x52C75ad49024dB2474cB8a581F625bee2E14e8E7
 const account = web3.eth.accounts.privateKeyToAccount(PVT_KEY);
 
-const userAddress = "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"; // @dev -  dummy user address replace this while calling from API
+const userAddress = "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"; // @dev -  require user address from F.E.
 
 const claimMessage = async (userAddress) => {
   const balance = await web3.eth.getBalance(userAddress);
@@ -21,7 +22,12 @@ const claimMessage = async (userAddress) => {
     { type: "uint256", value: epochTime }
   );
 
-  console.log("CLAIM:${userAddress}:${epochTime}:", text);
+  fs.appendFileSync(
+    "./claim_log.txt",
+    `\nCLAIM:${userAddress}:${balance}:${epochTime}`
+  );
+
+  console.log("CLAIM:${userAddress}:${balance}:${epochTime}", text);
   return text;
 };
 
@@ -33,5 +39,6 @@ const signMessage = (message) => {
 claimMessage(userAddress).then((message) => {
   signature = signMessage(message);
   console.log(` { message, signature }:`, { message, signature });
+
   return { message, signature };
 });
